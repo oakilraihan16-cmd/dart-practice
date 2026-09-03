940985f7bcedf04dea9f267f63320b69dbe3796b
oakilraihan16-cmd <oakilraihan16@gmail.com>
2026-09-04 00:39:44 +0600

    dart class 1 module 1

---
+++ practice_class_1.dart
@@ -0,0 +1,32 @@
+void main(){
+  print('Hello World');
+  print('Hello');
+
+  String name ='Wakil Raihan';
+  int age = 25;
+  double cgpa = 3.75;
+  bool isMale = true;
+  print(name);
+
+  name ='wakil';
+  
+
+  print('name: $name, age: $age, cgpa: $cgpa, isMale: $isMale');
+
+
+  var a = 'sabbir';
+  print(a);
+
+  dynamic b ='xyz';
+  print(b);
+  b = 100;
+  print(b);
+  b = true;
+  print(b);
+
+  final c = 25;
+  print(c);
+
+  const pi = 3.1416;
+  print(pi);
+}
